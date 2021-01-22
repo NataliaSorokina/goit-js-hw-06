9e982319973befd3427de87ed3fd688110fef112
Natalia Sorokina <pticasonabird@gmail.com>
2021-01-22 20:40:51 +0300

    исправления в задании 9

--- js/tasks.js
+++ js/tasks.js
@@ -81,9 +81,12 @@ console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sher
 //Задание 9
 //Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
 
-const getNamesSortedByFriendsCount = users => users
-  .sort((a, b) => a.friends.length - b.friends.length)
-  .map(user => user.name);
+const getNamesSortedByFriendsCount = users => {
+  return [...users]
+    .sort((a, b) => a.friends.length - b.friends.length)
+    .map(user => user.name);
+};
+  
 
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
